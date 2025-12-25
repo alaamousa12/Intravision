54e66d2ddb5adb5d13f1e3a4271ad28be62046d5
Alaa_mousa <am8182968@gmail.com>
2025-12-25 03:19:06 +0200

    create file splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intravision/features/splash/splash_screen.dart';
+import 'package:intravision/features/splash/presentation/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
